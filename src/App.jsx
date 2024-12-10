c1038c9abef0b1fe6b0fe7b29fc1a63e9a7e273d
Katie Nink Smith <katiej.smith@outlook.com>
2024-12-10 14:00:52 -0700

    everything is functioning

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Learners from "./components/Learners";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hey hey hey! ALAB 320.3.1</h1>
+        <Learners />
+      </div>
+    </>
+  );
+}
+
+export default App;
